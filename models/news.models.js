3618f726a8e21255fb1abe91b87a44ab3004bb89
Sanya Hussain <sanz_133@hotmail.com>
2024-01-19 13:34:33 +0000

    9 finished

--- models/news.models.js
+++ models/news.models.js
@@ -73,3 +73,15 @@ exports.setVotes = (article_id, inc_votes) => {
       return rows[0];
     });
 };
+
+exports.findCommentByCommentId = (comment_id) => {
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [
+      comment_id,
+    ])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Comment not found" });
+      }
+    });
+};
